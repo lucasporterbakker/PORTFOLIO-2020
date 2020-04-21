545ea0857a0521d76e0e1530be6deb7d20cf4096
Lucas Porter-Bakker <lucas.porter.bakker@gmail.com>
2020-04-20 23:14:24 -0400

    button

---
+++ js/rocket.js
@@ -0,0 +1,44 @@
+
+// Birds:
+document.addEventListener("DOMContentLoaded", function(){
+      var el = document.querySelector(".button-bird");
+      var text = document.querySelector(".button-bird__text");
+          el.addEventListener('click', function() {
+            el.classList.toggle('active');
+
+            if(el.classList.contains('active')){
+            	console.log('true');
+            	text.innerHTML = 'DONE';
+            }else{
+            	text.innerHTML = 'SEND';
+            }
+        });
+    });
+
+// Rocket:
+// const d = 40;
+//
+// document.querySelectorAll('.rocket-button').forEach(elem => {
+//
+//     elem.querySelectorAll('.default, .success > div').forEach(text => {
+//         charming(text);
+//         text.querySelectorAll('span').forEach((span, i) => {
+//             span.innerHTML = span.textContent == ' ' ? '&nbsp;' : span.textContent;
+//             span.style.setProperty('--d', i * d + 'ms');
+//             span.style.setProperty('--ds', text.querySelectorAll('span').length * d - d - i * d + 'ms');
+//         });
+//     });
+//
+//     elem.addEventListener('click', e => {
+//         e.preventDefault();
+//         if(elem.classList.contains('animated')) {
+//             return;
+//         }
+//         elem.classList.add('animated');
+//         elem.classList.toggle('live');
+//         setTimeout(() => {
+//             elem.classList.remove('animated');
+//         }, 2400);
+//     });
+//
+// });
